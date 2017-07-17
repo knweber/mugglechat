e7cd2ca3b4e3c5860661c463729beb5a032f2e1f
Kris <kris.weber.1994@gmail.com>
2017-07-16 20:30:24 -0700

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true
+  validates :email, presence: true
+
   has_many :posts
 
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
