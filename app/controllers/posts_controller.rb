5da3935457638fd68bf157e0e0280603d36ac3cb
Kris <kris.weber.1994@gmail.com>
2017-07-16 18:41:49 -0700

    add CRUD routes to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,47 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:edit, :update, :show, :delete]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new(params[:post])
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to post_path(@posts)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(params[:post])
+      redirect_to post_path(@posts)
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
 end
