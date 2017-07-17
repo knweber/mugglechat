c11aeac6718d8daddbda88702336b1ffa161e286
Kris <kris.weber.1994@gmail.com>
2017-07-16 20:22:45 -0700

    add view folder for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,16 @@
 class UsersController < ApplicationController
 
-  before_action :find_post, only: [:edit, :update, :show, :delete]
-
   def new
-    @user = User.new(params.require(:user).permit(:title, :body))
+    @user = User.new(params[:user])
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body))
-    @post.save
-    redirect_to @post
+    @user = User.new(params[:user])
+    @user.save
+    redirect_to @user
   end
 
   def show
-  end
-
-  private
-
-  def find_user
     @user = User.find(params[:id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'posts/index'
 
   resources :posts
+  resources :users
 
   root to: "posts#index"
 
